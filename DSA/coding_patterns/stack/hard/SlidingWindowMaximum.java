33dfd8b7feca33306ee02a8ec7c1b688eef8084a
lakshmansinghmalviya <lslakshman2024@gmail.com>
2026-01-13 01:09:29 +0530

    Done with the maximum in the window

---
+++ DSA/coding_patterns/stack/hard/SlidingWindowMaximum.java
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/sliding-window-maximum/description/
+
+package coding_patterns.stack.hard;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class SlidingWindowMaximum {
+
+    public int[] maxSlidingWindow(int[] arr, int k) {
+
+        LinkedList<Integer> stack = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            // if window is going outside
+            if (!stack.isEmpty() && stack.getFirst() <= i - k) {
+                stack.removeFirst();
+            }
+
+            // remove if we get the smaller smaller element before and we are in window
+            // already
+            while (!stack.isEmpty() && arr[stack.getLast()] <= arr[i]) {
+                stack.removeLast();
+            }
+
+            stack.addLast(i);
+            if (i >= k - 1)
+                list.add(arr[stack.getFirst()]);
+        }
+
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+
+    public static void main(String[] args) {
+        // LinkedList<Integer> queue = new LinkedList<>();
+        // queue.addLast(1);
+        // queue.addLast(2);
+        // queue.addLast(3);
+        // queue.addLast(1);
+        // System.out.println(queue);
+        // queue.removeFirst();
+        // System.out.println(queue);
+        // queue.removeLast();
+        // queue.getFirst();
+        // queue.getLast();
+        // System.out.println(queue);
+    }
+}
