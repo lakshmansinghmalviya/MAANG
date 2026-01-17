5f575202149ff4bcb79b4efc604af7af1cf833b9
lakshmansinghmalviya <lslakshman2024@gmail.com>
2026-01-17 17:43:26 +0530

    Done with the longest substring with atmost k distinct chars

---
+++ DSA/coding_patterns/sliding_window_2_pointer/LongestSubStringWithKDinstinctChars.java
@@ -0,0 +1,34 @@
+// https://takeuforward.org/plus/dsa/problems/longest-substring-with-at-most-k-distinct-characters
+
+package coding_patterns.sliding_window_2_pointer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubStringWithKDinstinctChars {
+
+    int longestSubStringWithKDinstinctChars(String s, int k) {
+
+        Map<Character, Integer> map = new HashMap<>();
+        int l = 0, r = 0;
+        int max = 0;
+        while (r < s.length()) {
+            map.put(s.charAt(r), map.getOrDefault(s.charAt(r), 0) + 1);
+
+            while (map.size() > k) {
+                map.put(s.charAt(l), map.getOrDefault(s.charAt(l), 0) - 1);
+                if (map.getOrDefault(s.charAt(l), 0) <= 0) {
+                    map.remove(s.charAt(l));
+                }
+                l++;
+            }
+            max = Math.max(max, r - l + 1);
+            r++;
+        }
+        return  max;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
