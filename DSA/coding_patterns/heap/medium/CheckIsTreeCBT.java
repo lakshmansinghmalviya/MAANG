d1db6b019f47757d664c1c49d099abb4e7234d27
lakshmansinghmalviya <lslakshman2024@gmail.com>
2026-01-20 02:22:25 +0530

    Done with the CBT check

---
+++ DSA/coding_patterns/heap/medium/CheckIsTreeCBT.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/check-completeness-of-a-binary-tree/
+
+package coding_patterns.heap.medium;
+
+public class CheckIsTreeCBT {
+    int totalNode(TreeNode root) {
+        if (root == null)
+            return 0;
+        int left = totalNode(root.left);
+        int right = totalNode(root.right);
+        return 1 + left + right;
+    }
+
+    public boolean isCompleteTree(TreeNode root, int n, int i) {
+        if (root == null)
+            return true;
+        if (i > n)
+            return false;
+        boolean left = isCompleteTree(root.left, n, i * 2);
+        boolean right = isCompleteTree(root.right, n, i * 2 + 1);
+        return left & right;
+    }
+
+    public boolean isCompleteTree(TreeNode root) {
+        int n = totalNode(root);
+        int i = 1;
+        return isCompleteTree(root, n, 1);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
