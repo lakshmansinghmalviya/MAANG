8f3e7642dba548b7342025b66a33cef83bbd5f9b
lakshmansinghmalviya <lslakshman2024@gmail.com>
2026-01-20 01:11:43 +0530

    Done with the kth largest optimized

--- DSA/coding_patterns/heap/medium/KthLargestElementInArray.java
+++ DSA/coding_patterns/heap/medium/KthLargestElementInArray.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class KthLargestElementInArray {
+
     public int findKthLargest(int[] nums, int k) {
         // using sorting it will take o(n log n) -T
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
@@ -16,8 +17,23 @@ public class KthLargestElementInArray {
 
         return pq.peek();
     }
+    
+    public int findKthLargestOptimized(int[] nums, int k) {
+        // using sorting it will take o(n log n) -T
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < k; i++)
+            pq.offer(nums[i]);
 
+        for (int i = k; i < nums.length; i++) {
+            if (nums[i] > pq.peek()) {
+                pq.poll();
+                pq.offer(nums[i]);
+            }
+        }
+
+        return pq.peek();
+    }
     public static void main(String[] args) {
-
+        // by default it works for the min heap
     }
 }
