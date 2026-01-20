ed57bed3b3051c83ce44205d081861d690408bc2
LakshmanSinghM <lslakshman2024@gmail.com>
2026-01-21 01:59:57 +0530

    Done with the connnect rops to in min cost

---
+++ DSA/coding_patterns/heap/medium/ConnectNRopes.java
@@ -0,0 +1,33 @@
+// https://www.geeksforgeeks.org/problems/minimum-cost-of-ropes-1587115620/1
+
+package coding_patterns.heap.medium;
+
+import java.util.PriorityQueue;
+
+public class ConnectNRopes {
+    public static int minCost(int[] arr) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int n : arr)
+            pq.offer(n);
+        int sum = 0;
+
+        if (pq.size() == 1)
+            return 0;
+
+        while (pq.size() > 1) {
+            int first = pq.poll();
+            int second = pq.poll();
+            sum = sum + first + second;
+            // System.out.print(sum+" ");
+            // System.out.print(sum+" ");
+            pq.offer(first + second);
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
