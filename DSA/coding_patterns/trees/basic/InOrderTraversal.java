0a12860b5932ebdebd3974c8c979e8cc66789639
LakshmanSinghM <lslakshman2024@gmail.com>
2026-02-02 02:25:20 +0530

    Done with the inorder using the stack

--- DSA/coding_patterns/trees/basic/InOrderTraversal.java
+++ DSA/coding_patterns/trees/basic/InOrderTraversal.java
@@ -4,14 +4,34 @@ package coding_patterns.trees.basic;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class InOrderTraversal {
-        void inOrderList(List<Integer> list, TreeNode root) {
+    void inOrderList(List<Integer> list, TreeNode root) {
         if (root == null)
             return;
         inOrderList(list, root.left);
         list.add(root.val);
         inOrderList(list, root.right);
+    }
+
+    // Using recursive solution
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (true) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                if (stack.isEmpty())
+                    break;
+                root = stack.pop();
+                list.add(root.val);
+                root = root.right;
+            }
+        }
+        return list;
     }
 
     public List<Integer> inorderTraversal(TreeNode root) {
--- DSA/coding_patterns/trees/basic/PreOrderTraversalOrDFS.java
+++ DSA/coding_patterns/trees/basic/PreOrderTraversalOrDFS.java
@@ -4,9 +4,33 @@ package coding_patterns.trees.basic;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class PreOrderTraversalOrDFS {
-        void preOrderList(List<Integer> list, TreeNode root) {
+
+    // we can solve it using stack also
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null)
+            return list;
+        // usual formula is root, left, right
+        // in stack just replace , get the node and then put rigth, left
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            list.add(node.val);
+            if (node.right != null)
+                stack.push(node.right); // first put right and then left
+            if (node.left != null)
+                stack.push(node.left);
+        }
+        return list;
+    }
+
+    void preOrderList(List<Integer> list, TreeNode root) {
         if (root == null)
             return;
         list.add(root.val);
