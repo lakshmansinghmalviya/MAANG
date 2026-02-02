76019e13d9d8f997f879e0d0b8e2a3727316b90a
LakshmanSinghM <lslakshman2024@gmail.com>
2026-02-03 01:11:22 +0530

    Done with the linkedlist

--- DSA/coding_patterns/trees/basic/PostOrderTraversal.java
+++ DSA/coding_patterns/trees/basic/PostOrderTraversal.java
@@ -3,10 +3,13 @@
 package coding_patterns.trees.basic;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class PostOrderTraversal {
-       void postOrderList(List<Integer> list, TreeNode root) {
+
+    void postOrderList(List<Integer> list, TreeNode root) {
         if (root == null)
             return;
         postOrderList(list, root.left);
@@ -14,10 +17,35 @@ public class PostOrderTraversal {
         list.add(root.val);
     }
 
- 
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
         postOrderList(list, root);
+        return list;
+    }
+
+    public List<Integer> postorderTraversalUsingTwoStack(TreeNode root) {
+        List<Integer> list = new LinkedList<>();
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+
+        if (root == null)
+            return list;
+
+        stack1.push(root);
+
+        while (!stack1.isEmpty()) {
+            TreeNode temp = stack1.pop();
+            stack2.push(temp);
+
+            TreeNode taken = stack2.peek();
+            list.addFirst(taken.val);
+            if (taken.left != null) {
+                stack1.push(taken.left);
+            }
+            if (taken.right != null) {
+                stack1.push(taken.right);
+            }
+        }
         return list;
     }
 }
