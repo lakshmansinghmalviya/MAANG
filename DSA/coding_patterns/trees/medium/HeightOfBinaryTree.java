d6ed1af91bfc08a0d65942e48cc16f981678a7c4
LakshmanSinghM <lslakshman2024@gmail.com>
2026-02-04 02:29:01 +0530

    Done with starting the medium level of the binary trees

--- DSA/coding_patterns/trees/medium/HeightOfBinaryTree.java
+++ DSA/coding_patterns/trees/medium/HeightOfBinaryTree.java
@@ -8,11 +8,16 @@ public class HeightOfBinaryTree {
     public int maxDepth(TreeNode root) {
         if (root == null)
             return 0;
-        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+
+        // or better readablity
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return 1 + Math.max(left, right);
+        // return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 
     public static void main(String[] args) {
 
     }
-
 }
