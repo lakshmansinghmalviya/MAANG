b62532bbca18d98cbc9b1afce032f55293e7beba
LakshmanSinghM <lslakshman2024@gmail.com>
2026-02-11 00:53:55 +0530

    DOne with other approach for the vertical traversal

--- DSA/coding_patterns/trees/hard/VerticalOrderTraversal.java
+++ DSA/coding_patterns/trees/hard/VerticalOrderTraversal.java
@@ -30,8 +30,12 @@ public class VerticalOrderTraversal {
 
     // we can use any traversal order just do the -1 if goes to left and +1 if right
 
+    // we can use any traversal order just do the -1 if goes to left and +1 if right
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         // vertical ind level ind
+        // treemap need to maintain from left to right so most neg to pos
+        // the inner treemap we need to maintain the top to bottom traversal for the
+        // level one
         TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
         Queue<Tuple> q = new LinkedList<>();
         q.offer(new Tuple(root, 0, 0));
@@ -44,9 +48,11 @@ public class VerticalOrderTraversal {
 
             if (!map.containsKey(x)) {
                 map.put(x, new TreeMap<>());
+                // putifabsent we can use
             }
 
             if (!map.get(x).containsKey(y)) {
+                // putifabsent we can use
                 map.get(x).put(y, new PriorityQueue<>());
             }
 
@@ -75,6 +81,48 @@ public class VerticalOrderTraversal {
 
         return lists;
     }
+
+    // public List<List<Integer>> verticalTraversal(TreeNode root) {
+    // // This solution will work when the order level doesn't matter top to bottom
+    // must be in this prob
+    // //vertical ind level ind
+    // TreeMap<Integer, PriorityQueue<Integer>> map = new TreeMap<>();
+    // Queue<Tuple> q = new LinkedList<>();
+    // q.offer(new Tuple(root, 0, 0));
+
+    // while (!q.isEmpty()) {
+    // Tuple tuple = q.poll();
+    // TreeNode node = tuple.node;
+    // int x = tuple.vertical;
+    // int y = tuple.level;
+
+    // if (map.get(x) == null) {
+    // // putifabsent we can use
+    // map.put(x, new PriorityQueue<>());
+    // }
+
+    // map.get(x).offer(node.val);
+
+    // if (node.left != null) {
+    // q.offer(new Tuple(node.left, x - 1, y + 1));
+    // }
+    // if (node.right != null) {
+    // q.offer(new Tuple(node.right, x + 1, y + 1));
+    // }
+    // }
+
+    // List<List<Integer>> lists = new ArrayList<>();
+    // System.out.println(map);
+    // for (PriorityQueue<Integer> pq : map.values()) {
+    // List<Integer> list = new ArrayList<>();
+    // while (!pq.isEmpty()) {
+    // System.out.println(pq.peek());
+    // list.add(pq.poll());
+    // }
+    // lists.add(list);
+    // }
+    // return lists;
+    // }
 
     public static void main(String[] args) {
 
